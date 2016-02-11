e4d654f70d54fa5ec1007e664919b5c6804caed7
vandosant <sjskender@gmail.com>
2016-02-11 13:21:02 -0700

    Implement logging of times for numbers

--- web/js/pages/metronome.js
+++ web/js/pages/metronome.js
@@ -1,5 +1,4 @@
 (function (context) {
-
   function init() {
     var SpeechRecognition = webkitSpeechRecognition;
     var SpeechGrammarList = webkitSpeechGrammarList;
@@ -8,22 +7,30 @@
     var speechRecognitionList = new SpeechGrammarList();
     speechRecognitionList.addFromString(grammar, 1);
     recognition.grammars = speechRecognitionList;
+    recognition.interimResults = true;
     document.body.onclick = function () {
       recognition.start();
-      console.log('Ready to receive a color command.');
+      console.log('Ready to receive a command.');
     };
-
+    var startTime = null;
+    var eventTime = [];
+    var started = false;
+    var eventNum = 0;
     recognition.onresult = function (event) {
-      var color = event.results[0][0].transcript;
-      console.log('Result received:', color);
-      console.log('Confidence:', event.results[0][0].confidence);
+      var transcript = event.results[0][0].transcript;
+      if (transcript === 'one') {
+        startTime = event.timeStamp;
+        started = true;
+      }
+      if (started && transcript == '12' || transcript == '123' || transcript == '1234') {
+        eventNum++;
+        eventTime.push(event.timeStamp);
+      }
     };
-    recognition.onspeechstart = function (event) {
-      console.log('spst')
-    };
-    recognition.onsaudiostart = function (event) {
-      console.log('sdst')
-    };
+    recognition.onspeechend = function(event) {
+      var totalDistance = eventTime.reduce(function(a, b) {return a + b})
+      console.log((totalDistance / eventNum) - startTime);
+    }
     recognition.onerror = function (event) {
       console.log('error', event.data)
     };
